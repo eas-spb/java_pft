60b79d66542b5fa0e3c8051dc87f7d534318edc0
eas1989 <84876407+eas1989@users.noreply.github.com>
2021-05-27 16:23:37 -0700

    start

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTest.java
@@ -0,0 +1,12 @@
+package ru.stqa.pft.sandbox;
+
+import com.sun.tracing.dtrace.Attributes;
+import org.testng.annotations.Test;
+
+public class SquareTest {
+
+    @Test
+    public void testArea() {
+        System.out.println("Hello");
+    }
+}
